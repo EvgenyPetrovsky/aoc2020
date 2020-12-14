41ead062c3e3c24d96d7f323400dcc9c18c145cc
Evgeny Petrovsky <evgeny.petrovsky@ing.com>
2020-12-14 17:58:41 +0100

    another attempt

--- day13.R
+++ day13.R
@@ -35,6 +35,22 @@ waiting_time <- function(start_time, bus_id) {
   else bus_id - time_ago
 }
 
+gcd <- function(a, b) {
+  if (b == 0) a
+  else gcd(b, a %% b)
+}
+
+lcm <- function(a, b) as.double(abs(a*b) / gcd(a, b))
+
+iterations_to_zero <- function(start, inc, div) {
+  i <- 1
+  while (start != 0) {
+    start <- (start + inc) %% div
+    i <- i + 1
+  }
+  i
+}
+
 #- SOLUTION PART 1 ------------------------------------------------------------#
 
 day13_part1_solution <- function(input) {
@@ -62,6 +78,10 @@ print(real_result_part1)
 
 #- SOLUTION PART 2 ------------------------------------------------------------#
 
+#' idea for new solution: start with inc = 1 and find a number when 1st element is 1
+#' continue with inc = lcm(1st) and find number when 2nd el is 2
+#' continue with inc = lcm(1st, 2nd) and find a number when 3rd el is 3, ... until all numbers are in
+
 day13_part2_solution <- function(input, start_time = 0) {
   busses <- input_busses_part2(input)
   num_busses <- length(busses)
@@ -72,27 +92,22 @@ day13_part2_solution <- function(input, start_time = 0) {
     by = 1,
     length.out = num_busses)
 
-  filter_busses <- (busses != 1 & busses != max_bus_id)
+  filter_busses <- (busses != 1)
   busses_filtered <- busses[filter_busses]
   diff_times_filtered <- diff_times[filter_busses]
-
-  time_check <- FALSE
   time <- as.double(start_time - (start_time %% max_bus_id))
-  diff_times_filtered <- as.integer((time + diff_times_filtered)%% busses_filtered)
-  i <- 0
-  i_print <- 10
-  while (!time_check) {
-    i <- i + 1
-    if (i == i_print) {
-      i_print <- i_print * 10
-      print(paste("iteration", i))
-      print(paste("time", time, "system time", Sys.time()))
-    }
-    time <- (time + max_bus_id)
-    diff_times_filtered <- (diff_times_filtered + max_bus_id) %% busses_filtered
-    time_check <- all(diff_times_filtered == 0)
-  }
-  winning_start_time <- time - max_bus_idx + 1
+  diff_times_filtered <- as.integer((time + diff_times_filtered) %% busses_filtered)
+  
+  lcm_input <- mapply(
+    FUN = function(x, y) 
+      iterations_to_zero(x, max_bus_id, y), 
+    diff_times_filtered, busses_filtered,
+    SIMPLIFY = T, USE.NAMES = F
+    )
+  
+  iterations_to_all_zero <- Reduce(x = lcm_input, f = lcm)
+  
+  winning_start_time <- time + (iterations_to_all_zero * max_bus_id) - max_bus_idx + 1
   winning_start_time
 }
 
@@ -123,10 +138,10 @@ print(paste(
   "test result:", test_result,
   "valid:", test_result == 1261476))
 
-test_result <- day13_part2_solution(c("X", "1789,37,47,1889"), 20000)
+test_result <- day13_part2_solution(c("X", "1789,37,47,1889"), 0)
 print(paste(
   "test result:", test_result,
   "valid:", test_result == 1202161486))
 
 real_result_part2 <- day13_part2_solution(real_input, 100000000000000)
-print(real_result_part2)
+print(format(real_result_part2, scientific = F))

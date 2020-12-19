c500f295605a60bf03675097c09e466055025d65
Evgeny Petrovsky <ev.petrovsky@gmail.com>
2020-12-19 16:55:54 +0100

    day 16 part 2 solved

--- day16.R
+++ day16.R
@@ -15,6 +15,20 @@ test_input <- c(
 "38,6,12"
 )
 
+test_input_2 <- c(
+  "class: 0-1 or 4-19",
+  "row: 0-5 or 8-19",
+  "seat: 0-13 or 16-19",
+  "",
+  "your ticket:",
+  "11,12,13",
+  "",
+  "nearby tickets:",
+  "3,9,18",
+  "15,1,5",
+  "5,14,9"
+)
+
 real_input <- readLines("./inputs/day16-input.txt")
 
 #- LOGIC ----------------------------------------------------------------------#
@@ -31,7 +45,7 @@ constraint_text_to_ranges <- function(constraint_text) {
     strsplit(split = "-") %>%
     Map(f = function(x) x %>% unlist() %>% as.integer()) %>%
     Reduce(f = function(z, x) {
-      z[x[1]:x[2]] <- TRUE
+      z[(x[1]+1):(x[2]+1)] <- TRUE
       z
       }, init = rep(FALSE, 1000))
 }
@@ -58,9 +72,9 @@ parse_input <- function(input) {
       z[[constraint_text_to_label(x)]] <- constraint_text_to_ranges(x)
       z
     }, init = list())
-  new_input[["you"]] <- ticket_text_to_numbers(your_ticket_input)
+  new_input$you <- ticket_text_to_numbers(your_ticket_input)
 
-  new_input[["neighbors"]] <-
+  new_input$neighbors <-
     neighbor_ticket_input %>%
     strsplit(split = ",") %>%
     Map(f = as.integer)
@@ -70,8 +84,8 @@ parse_input <- function(input) {
 
 check_number_conforms_constraints <- function(number, constraints) {
   constraints %>%
-    Map(f = function(x) x[number]) %>%
-    # number should meet any of the requirements / constraints / ranges
+    Map(f = function(x) x[number+1]) %>%
+    # number should meet at least one constraint
     Reduce(f = any)
 }
 
@@ -83,18 +97,30 @@ check_ticket_conforms_constraints <- function(ticket, constraints) {
 }
 
 find_ticket_invalid_number <- function(ticket, constraints) {
-  valid_numbers <- 
+  valid_numbers <-
     ticket %>%
     Map(f = function(x) check_number_conforms_constraints(x, constraints)) %>%
     unlist()
-  
+
   ticket[valid_numbers == FALSE]
 }
+
+number_position_for_constraint <- function(constraint, tickets) {
+  positions <-
+      tickets %>%
+      Map(f = function(x) constraint[(x+1)]) %>%
+      Reduce(f = function(z, x) {z & x}) %>%
+      which()
+  
+  #positions[1]
+  positions
+}
+
 #- SOLUTION PART 1 ------------------------------------------------------------#
 
 day16_part1_solution <- function(input) {
   ticket <- parse_input(input)
-  
+
   ticket$neighbors %>%
     Map(f = function(x) find_ticket_invalid_number(x, ticket$constraints)) %>%
     Reduce(f = sum)
@@ -112,15 +138,45 @@ print(format(real_result_part1, scientific = FALSE))
 #- SOLUTION PART 2 ------------------------------------------------------------#
 
 day16_part2_solution <- function(input) {
-  NULL
+  ticket <- parse_input(input)
+
+  all_tickets <- c(list(ticket$you), ticket$neighbors)
+  all_valid_tickets <-
+    all_tickets %>%
+    Filter(f = function(x) {
+      check_ticket_conforms_constraints(x, ticket$constraints)
+    })
+
+  field_types_map <-
+    ticket$constraints %>%
+    Map(f = function(x) number_position_for_constraint(x, all_valid_tickets))
+  
+  sorted_fields <- field_types_map %>% 
+    Map(f = length) %>% 
+    unlist %>% 
+    sort() %>% 
+    names()
+  
+  # we need to exclude ambiguous results by starting with field that has only 1 
+  # column and eclude that columns from other fieldss
+  field_positions <- sorted_fields %>%
+    Reduce(f = function(z, x) {
+      pos <- z[[x]]
+      Map(z, f = function(x) {
+        if (all(x == pos)) x 
+        else x[x != pos]
+      })
+    }, init = field_types_map)
+  
+  departure_field_positions <-
+    field_positions[grepl(pattern = "departure", names(field_types_map))] %>%
+    unlist()
+
+  ticket$you %>% magrittr::extract(departure_field_positions) %>% prod()
+
 }
 
-test_output_part2 <- -1
-test_result <- day16_part2_solution(test_input)
-
-print(paste(
-  "test result:", test_result,
-  "valid:", test_result == test_output_part2))
+test_result <- day16_part2_solution(test_input_2)
 
 real_result_part2 <- day16_part2_solution(real_input)
 print(format(real_result_part2, scientific = FALSE))
